c160da35da81fadd8890b65121514aedadb9f69e
Fasteol <razanmfs507@gmail.com>
2026-05-29 15:43:37 +0700

    commit pertama: membuat portofolio menggunakan react

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,26 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Portofolio from "../pages/portofolio";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Portofolio - Razan" },
+    {
+      name: "description",
+      content: "Web Developer Portfolio",
+    },
+  ];
+}
+
+export function links() {
+  return [
+    {
+      rel: "icon",
+      type: "image/svg+xml",
+      href: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚡</text></svg>",
+    },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <Portofolio />;
 }
